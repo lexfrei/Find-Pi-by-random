ce62c057773689569819dbe027b46878c46d7957
Aleksey Sviridkin <f@lex.la>
2016-07-16 06:23:09 +0300

    Revert "realPi switched to math.Pi"
    This reverts commit 1b677cdc20c07698733e383ba122f0afade59070.

--- main.go
+++ main.go
@@ -28,6 +28,8 @@ import (
 	"time"
 )
 
+const realPi float64 = 3.14159265358979323846
+
 var r int
 var scan int
 
@@ -60,6 +62,6 @@ func main() {
 	elapsed := time.Since(start)
 	pi := float32(inS) * 4 / float32(r) * float32(r) / float32(scan)
 	fmt.Printf("Pi\t = %f\n", pi)
-	fmt.Printf("Accuracy = %f\n", 1-math.Abs(float64(pi)-math.Pi)/math.Pi)
+	fmt.Printf("Accuracy = %f\n", 1-math.Abs(float64(pi)-realPi)/realPi)
 	fmt.Print("Time\t = ", elapsed, "\n")
 }
